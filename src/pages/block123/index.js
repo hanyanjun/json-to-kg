c2a35ea8d4cf0888cf08ac18c4f66ba98441fa18
hanyanjun <HYJ_hanyanjun@163.com>
2021-07-11 21:50:29 +0800

    feat: add file

--- src/pages/block123/index.js
+++ src/pages/block123/index.js
@@ -1,19 +1,25 @@
 import React, { useEffect, useRef } from "react";
+import { ALL_LABELS, ALL_RELS } from "./match";
 const Neo4j =  require('neo4j-data');
 
 export default function(){
     let neo4j = useRef(null);
-    useEffect(()=>{
+    useEffect( async ()=>{
         neo4j.current = new Neo4j();
         neo4j.current.config({
-            url : 'bolt://124.156.168.171:7687',
+            url : 'bolt://124.156.168.171:17687',
             username : 'neo4j',
             password : 'llps&789'
         });
-        neo4j.current.runMatch('call db.labels()').then(res=>{
-            console.log('call db.labels()' , res);
-        })
+        // let result =  await neo4j.current.runMatch(ALL_LABELS);
+        // let labels = result.records.map(v=>{
+        //     return {key : v._fields[0] , value : v._fields[0]}
+        //   });
+        //   console.log('all labels' , labels)
+          let rels = await neo4j.current.runMatch(ALL_RELS);
+          console.log('all data' , neo4j.current.dataDetail(rels.records))
     },[])
+    
 
 
     return(
---
+++ src/pages/block123/match.js
@@ -0,0 +1,14 @@
+
+
+// 所有macth语句汇总
+
+
+
+const ALL_LABELS = 'call db.labels()';
+
+const ALL_RELS = 'MATCH p=()-->() RETURN p LIMIT 25'
+
+export {
+    ALL_LABELS,
+    ALL_RELS
+}
