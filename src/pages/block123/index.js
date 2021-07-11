838fc4046b1de6e96fc7b5770e3b2f222027bc59
hanyanjun <HYJ_hanyanjun@163.com>
2021-07-11 20:16:49 +0800

    feat: build

--- src/pages/block123/index.js
+++ src/pages/block123/index.js
@@ -1,9 +1,21 @@
-import React from "react";
-
-
-
+import React, { useEffect, useRef } from "react";
+const Neo4j =  require('neo4j-data');
 
 export default function(){
+    let neo4j = useRef(null);
+    useEffect(()=>{
+        neo4j.current = new Neo4j();
+        neo4j.current.config({
+            url : 'bolt://124.156.168.171:7687',
+            username : 'neo4j',
+            password : 'llps&789'
+        });
+        neo4j.current.runMatch('call db.labels()').then(res=>{
+            console.log('call db.labels()' , res);
+        })
+    },[])
+
+
     return(
         <div>block123</div>
     )
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { HashRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from "../pages/home/Home"
 import Block123 from "../pages/block123"
 
